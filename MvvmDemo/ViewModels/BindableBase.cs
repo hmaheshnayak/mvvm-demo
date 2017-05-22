304fa739f024c01f6284b2694042225fb55eb70f
Mahesh Nayak <Mahesh Nayak>
2017-05-22 16:01:14 +0530

    First commit

---
+++ MvvmDemo/ViewModels/BindableBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmDemo.ViewModels
+{
+    class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void SetProperty<T>(ref T property, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(property, value))
+                return;
+
+            property = value;
+
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MvvmDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MvvmDemo.ViewModels
+{
+    class RelayCommand<T> : ICommand
+    {
+        private Action<T> execute;
+        private Func<T, bool> canExecute;
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object obj)
+        {
+            return canExecute == null || canExecute((T)obj);
+        }
+
+        public void Execute(object obj)
+        {
+            execute((T)obj);
+        }
+
+    }
+    class MainWindowViewModel : BindableBase
+    {
+        CustomerListViewModel customerViewModel;
+
+        OrderViewModel orderViewModel;
+
+        private BindableBase currentViewModel;
+
+        public BindableBase CurrentViewModel
+        {
+            get { return currentViewModel; }
+            set { SetProperty<BindableBase>(ref currentViewModel, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            customerViewModel = new CustomerListViewModel();
+            orderViewModel = new OrderViewModel();
+            NavCommand = new RelayCommand<string>(OnNav, null);
+        }
+
+        public RelayCommand<string> NavCommand { get; set; }   
+        
+        void OnNav(string viewName)
+        {
+            switch (viewName)
+            {
+                case "orders":
+                    CurrentViewModel = orderViewModel;
+                    break;
+                case "customers":
+                default:
+                    CurrentViewModel = customerViewModel;
+                    break;
+            }
+        }
+    }
+}
